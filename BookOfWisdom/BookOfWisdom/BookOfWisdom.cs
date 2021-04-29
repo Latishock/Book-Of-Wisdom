c0dfece88ea1bafd28c3b607e25c78ea9b69404a
Hikkaev <hikkaev@internet.ru>
2021-04-29 12:02:15 +0300

    Project creation + Singleton pattern
    --Project created.
    --Singleton patter was added it BookOfWisdom.Patterns namespace.

---
+++ BookOfWisdom/BookOfWisdom/BookOfWisdom.cs
@@ -0,0 +1,22 @@
+﻿namespace BookOfWisdom
+{
+    using UnityEngine;
+    namespace Patterns
+    {
+        public static class Singleton
+        {
+            public static void Initialize<T>(T instance, ref T instanceField, GameObject instanceGameObject)
+            {
+                if (instanceField == null)
+                {
+                    instanceField = instance;
+                }
+                else
+                {
+                    Object.Destroy(instanceGameObject);
+                }
+            }
+        }
+    }
+
+}
